50e2f93655e6c65b589aec569146874c3d094248
BlazeGamesYT <73793525+BlazeGamesYT@users.noreply.github.com>
2021-03-20 12:56:24 -0700

    Yay

---
+++ com/bryceblazegames/craftablestructures/craftableStructures.java
@@ -0,0 +1,15 @@
+package com.bryceblazegames.craftablestructures;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class craftableStructures extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
