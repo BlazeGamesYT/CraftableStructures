e0c10fed453e7ccc8d32d8a9f9daaa7ebd503404
BlazeGamesYT <73793525+BlazeGamesYT@users.noreply.github.com>
2021-03-20 16:47:31 -0700

    Milestone 1.1

--- com/bryceblazegames/craftablestructures/craftableStructures.java
+++ com/bryceblazegames/craftablestructures/craftableStructures.java
@@ -1,13 +1,16 @@
 package com.bryceblazegames.craftablestructures;
 
+import com.bryceblazegames.craftablestructures.commands.givebook;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.bryceblazegames.craftablestructures.items.itemManager;
 
 public class craftableStructures extends JavaPlugin {
 
     @Override
     public void onEnable() {
 
-
+        itemManager.init();
+        getCommand("givebook").setExecutor(new givebook());
 
     }
 
